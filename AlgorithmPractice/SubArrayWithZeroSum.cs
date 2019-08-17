8ffdc4b375bda85521cc93014d3a95468beb15b4
Keenan McNamara <keenan.mcnamara12@gmail.com>
2019-08-17 13:10:42 -0500

    Algorithms findings sub arrays that have a given sum

---
+++ AlgorithmPractice/SubArrayWithZeroSum.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AlgorithmPractice
+{
+    public class SubArrayWithZeroSum
+    {
+        /// <summary>
+        /// Checks if an array has any sub array set with a zero sum
+        /// </summary>
+        /// <param name="array">array to check</param>
+        /// <returns>true if there is a sub array with zero sum, false otherwise</returns>
+        public static bool Check(int[] array)
+        {
+            int runningSum = 0;
+
+            // Hash set so we can get an O(n) perf
+            HashSet<int> set = new HashSet<int>();
+            set.Add(0);     // make sure zero in the first node (potentially only) of the array still works
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                runningSum += array[i];
+
+                // If the current sum equals a past sum in all the sums we've seen, that means
+                // there's some sub array that adds to zero obviously (whatever is between
+                // the running sum and the sum in the hash set).
+                if (set.Contains(runningSum))
+                {
+                    return true;
+                }
+
+                set.Add(array[i]);
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// Find all sub arrays with zero sum (with array order being maintained)
+        /// </summary>
+        /// <param name="array">array to search for with sub arrays that add to zero</param>
+        /// <returns>list of sub arrays that add to zero</returns>
+        public static List<int[]> Find(int[] array)
+        {
+            int runningSum = 0;
+            List<int[]> resultList = new List<int[]>();
+
+            // Use running sum idea, but also know the index of the past sum and the current sum. Then we can 
+            // splice the array between those two indices to get all the sub arrays with zero sums.
+            // Map structure: <sum at index, index>
+            Dictionary<int, List<int>> hashMap = new Dictionary<int, List<int>>();
+
+            // Initialize with zero sum in case array starts with zero.
+            hashMap.Add(0, new List<int>() { -1 });
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                runningSum += array[i];
+
+                // Sum already found - build all possible arrays based on the sums we've already seen then add this index to the list for the next potential finding of this sum
+                if (hashMap.ContainsKey(runningSum))
+                {
+                    List<int> previousIndicesWithSum = hashMap[runningSum];
+                    foreach (int previousIndexWithSum in previousIndicesWithSum)
+                    {
+                        // start of the sum array is 1 index after the original finding of the sum to the new index.
+                        int startIndex = previousIndexWithSum + 1;
+                        int endIndex = i;
+                        int newArrayLength = endIndex - startIndex + 1;
+                        int[] subArray = new int[newArrayLength];
+                        Array.Copy(array, startIndex, subArray, 0, newArrayLength);
+                        resultList.Add(subArray);
+                    }
+
+                    // Add index for this sum to the list for next time we see this sum
+                    hashMap[runningSum].Add(i);
+                }
+                // Sum not seen yet - initialize a new list for this node 
+                else
+                {
+                    hashMap.Add(runningSum, new List<int>() { i });
+                }
+            }
+
+            return resultList;
+        }
+    }
+}
---
+++ AlgorithmPracticeTest/SubArrayWithZeroSumTest.cs
@@ -0,0 +1,74 @@
+﻿using AlgorithmPractice;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AlgorithmPracticeTests
+{
+    /// <summary>
+    /// Test class for SubArrayWithZeroSum.cs
+    /// </summary>
+    [TestClass]
+    public class SubArrayWithZeroSumTest
+    {
+        private readonly int[] TestArray1 = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        private readonly int[] TestArray2 = new int[] { 0 };
+        private readonly int[] TestArray3 = new int[] { 1, 2, 3, -6 };
+        private readonly int[] TestArray4 = new int[] { 1, 2, 3, -5, -1 };
+        private readonly int[] TestArray5 = new int[] { 1, 2, -5, -1, 3, 99 };
+        private readonly int[] TestArray6 = new int[] { 0, 1, 3, -4, 5, -5, 1 };
+        private readonly int[] TestArray7 = new int[] { 1, 2, 3, 0 };
+
+        [TestMethod]
+        public void Check_TestArray1_ReturnsFalse()
+        {
+            Assert.IsFalse(SubArrayWithZeroSum.Check(TestArray1));
+        }
+
+        [TestMethod]
+        public void Check_TestArray2_ReturnsTrue()
+        {
+            Assert.IsTrue(SubArrayWithZeroSum.Check(TestArray2));
+        }
+
+        [TestMethod]
+        public void Check_TestArray3_ReturnsTrue()
+        {
+            Assert.IsTrue(SubArrayWithZeroSum.Check(TestArray3));
+        }
+
+        [TestMethod]
+        public void Check_TestArray4_ReturnsTrue()
+        {
+            Assert.IsTrue(SubArrayWithZeroSum.Check(TestArray4));
+        }
+
+        [TestMethod]
+        public void Check_TestArray5_ReturnsTrue()
+        {
+            Assert.IsTrue(SubArrayWithZeroSum.Check(TestArray5));
+        }
+
+        [TestMethod]
+        public void Find_TestArray6_ReturnsTrue()
+        {
+            List<int[]> list = SubArrayWithZeroSum.Find(TestArray6);
+            Assert.IsTrue(list.Any(x => x.SequenceEqual(new int[] { 0, 1, 3, -4, 5, -5 })));
+            Assert.IsTrue(list.Any(x => x.SequenceEqual(new int[] { 0, 1, 3, -4, 5, -5 })));
+            Assert.IsTrue(list.Any(x => x.SequenceEqual(new int[] { 1, 3, -4, 5, -5 })));
+            Assert.IsTrue(list.Any(x => x.SequenceEqual(new int[] { 3, -4, 5, -5, 1 })));
+            Assert.IsTrue(list.Any(x => x.SequenceEqual(new int[] { 0, 1, 3, -4 })));
+            Assert.IsTrue(list.Any(x => x.SequenceEqual(new int[] { 1, 3, -4 })));
+            Assert.IsTrue(list.Any(x => x.SequenceEqual(new int[] { 5, -5 })));
+            Assert.IsTrue(list.Any(x => x.SequenceEqual(new int[] { 0 })));
+        }
+
+        [TestMethod]
+        public void Find_TestArray7_ReturnsTrue()
+        {
+            // Specifically looking for an array that just has a zero at any index
+            List<int[]> list = SubArrayWithZeroSum.Find(TestArray7);
+            Assert.IsTrue(list.Any(x => x.SequenceEqual(new int[] { 0 })));
+        }
+    }
+}
